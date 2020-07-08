8fc1ad255be590cd460f332d1c416543755e4a1d
Vasyl-007 <vasyl.manyo@gmail.com>
2020-07-08 12:50:51 +0300

    various inputs with material-ui

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import InputsAdornments from "./components/Inputs";
+
+function App() {
+  return (
+    <div>
+      <InputsAdornments />
+    </div>
+  );
+}
+
+export default App;
